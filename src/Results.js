db8b7c70c681d8398642e6c704dec9aa91bc8c2e
Rajat Bansal <rjtbansal@gmail.com>
2021-05-22 12:16:05 -0400

    component Results created

---
+++ src/Results.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import Pet from "./Pet";
+
+const Results = ({ pets }) => (
+  <div className="search">
+    {!pets.length ? (
+      <h2>No Pets Found</h2>
+    ) : (
+      pets.map((pet) => (
+        <Pet
+          name={pet.name}
+          animal={pet.animal}
+          breed={pet.breed}
+          key={pet.id}
+          location={`${pet.city}, ${pet.state}`}
+          id={pet.id}
+          images={pet.images}
+        />
+      ))
+    )}
+  </div>
+);
+
+export default Results;
--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { useState, useEffect } from "react";
 import Pet from "./Pet";
+import Results from "./Results";
 import useBreedList from "./useBreedList";
 
 const ANIMALS = ["bird", "cat", "dog", "rabbit", "reptile"];
@@ -26,7 +27,10 @@ const SearchParams = () => {
 
   return (
     <div className="search-params">
-      <form>
+      <form onSubmit={e => {
+        e.preventDefault();
+        requestPets();
+      }}>
         <label htmlFor="location">
           Location
           <input
@@ -70,14 +74,7 @@ const SearchParams = () => {
         </label>
         <button>Submit</button>
       </form>
-      {pets.map((pet) => (
-        <Pet
-          name={pet.name}
-          animal={pet.animal}
-          breed={pet.breed}
-          key={pet.id}
-        />
-      ))}
+     <Results pets = {pets} />
     </div>
   );
 };
