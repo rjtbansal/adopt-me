0e5ef7c8272823121fb1f0528b0cdd0e21660518
Rajat Bansal <rjtbansal@gmail.com>
2021-05-26 07:04:46 -0400

    added modals with portal

--- src/Details.js
+++ src/Details.js
@@ -2,12 +2,15 @@ import { useContext, useEffect, useState } from "react";
 import { withRouter } from "react-router";
 import Carousel from "./Carousel";
 import ErrorBoundary from "./ErrorBoundary";
+import Modal from "./Modal";
 import ThemeContext from "./ThemeContext";
 
 const Details = (props) => {
   const [loading, setLoading] = useState(true);
   const [petDetails, setPetDetails] = useState({});
   const [theme] = useContext(ThemeContext);
+  const [showModal, setShowModal] = useState(false);
+
   useEffect(() => {
     async function fetchDetails() {
       const res = await fetch(
@@ -21,7 +24,12 @@ const Details = (props) => {
   }, [props.match.params.id]);
 
   const { animal, breed, city, state, description, name, images } = petDetails;
+  console.log("petDetails: ", petDetails);
   //throw new Error; //uncomment to actually trigger the error by our ErrorBoundary
+
+  const toggleModal = () => setShowModal(!showModal);
+  const adopt = () => (window.location = "http://bit.ly/pet-adopt");
+
   return loading ? (
     <h2>Loading...</h2>
   ) : (
@@ -31,10 +39,23 @@ const Details = (props) => {
         <h1>{name}</h1>
         <h2>
           {animal} - {breed} - {city}, {state}
-          </h2>
-          {/* setting backgroundColor via our theme context color */}
-        <button style={{backgroundColor: theme}}>Adopt {name}</button>
+        </h2>
+        {/* setting backgroundColor via our theme context color */}
+        <button onClick={toggleModal} style={{ backgroundColor: theme }}>
+          Adopt {name}
+        </button>
         <p>{description}</p>
+        {showModal ? (
+          <Modal>
+            <div>
+              <h1>Would you like to adopt {name}</h1>
+              <div className="buttons">
+                <button onClick={adopt}>Yes</button>
+                <button onClick={toggleModal}>No</button>
+              </div>
+            </div>
+          </Modal>
+        ) : null}
       </div>
     </div>
   );
