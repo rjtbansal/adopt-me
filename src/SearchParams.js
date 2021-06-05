c4aa31b853554f4bc77db1eeb2487a9bc8ef464e
Rajat Bansal <rjtbansal@gmail.com>
2021-06-04 20:51:37 -0400

    tailwind components and breakpoints

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -34,20 +34,20 @@ const SearchParams = () => {
           requestPets();
         }}
       >
-        <label htmlFor="location">
+        <label className="search-label" htmlFor="location">
           Location
           <input
-            className="w-60 my-10"
+            className="search-control"
             id="location"
             onChange={(event) => setLocation(event.target.value)}
             value={location}
             placeholder="Location"
           />
         </label>
-        <label htmlFor="animal">
+        <label className="search-label" htmlFor="animal">
           Animal
           <select
-            className="w-60 my-10"
+            className="search-control"
             id="animal"
             value={animal}
             onChange={(event) => setAnimal(event.target.value)}
@@ -61,10 +61,11 @@ const SearchParams = () => {
             ))}
           </select>
         </label>
-        <label htmlFor="breed">
+        <label className="search-label" htmlFor="breed">
           Breed
           <select
-            className="w-60 my-10"
+            // we can use the className here which is defined as tailwind component in our style.css
+            className="search-control disabled:opacity-50"
             id="breed"
             value={breed}
             onChange={(event) => setBreed(event.target.value)}
@@ -78,10 +79,10 @@ const SearchParams = () => {
             ))}
           </select>
         </label>
-        <label htmlFor="theme">
+        <label className="search-label" htmlFor="theme">
           Theme
           <select
-            className="w-60 my-10"
+            className="search-control"
             value={theme}
             onChange={(e) => setTheme(e.target.value)}
             onBlur={(e) => setTheme(e.target.value)}
@@ -92,7 +93,12 @@ const SearchParams = () => {
             <option value="mediumorchid">Medium Orchid</option>
           </select>
         </label>
-        <button style={{ backgroundColor: theme }}>Submit</button>
+        <button
+          className="rounded px-6 py-2 text-white hover:opacity-50 border-none"
+          style={{ backgroundColor: theme }}
+        >
+          Submit
+        </button>
       </form>
       <Results pets={pets} />
     </div>
