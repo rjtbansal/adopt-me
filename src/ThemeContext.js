852a7c8e542ae92a1e26970e337fbf62b978dbe0
Rajat Bansal <rjtbansal@gmail.com>
2021-06-10 21:30:13 -0400

    config update for Eslint and typescript to work together, migrate ThemeContext to TS

--- src/ThemeContext.js
+++
@@ -1,6 +0,0 @@
-import { createContext } from "react";
-
-//to createContext we pass initial value and an empty function so it reads like hook
-const ThemeContext = createContext(["green", () => {}]);
-
-export default ThemeContext;
