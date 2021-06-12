506d73a37602ae63494efa2ff00fa6db222d62f0
Rajat Bansal <rjtbansal@gmail.com>
2021-06-12 12:09:32 -0400

    migrated Carousal and ErrorBoundary components to TS

--- src/Carousel.tsx
+++ src/Carousel.tsx
@@ -1,11 +1,18 @@
-import { useState } from "react";
+import { useState, FunctionComponent, MouseEvent, ReactNode } from "react";
 
-const Carousel = ({ images }) => {
+interface IProps {
+  images: string[];
+}
+
+const Carousel: FunctionComponent<IProps> = ({ images }) => {
   const [active, setActive] = useState(0);
 
-  const handleIndexClick = (event) => {
+  const handleIndexClick = (event: MouseEvent<HTMLElement>): void => {
     // reading our index property from img data-index. we skip data- and thats why its .dataset.index and not .dataset.data-index as one might imagine
     //below we get a string so by using + it converts to a number
+    if (!(event.target instanceof HTMLElement)) {
+      return;
+    }
     setActive(+event.target.dataset.index);
   };
 
--- src/ErrorBoundary.tsx
+++ src/ErrorBoundary.tsx
@@ -1,19 +1,27 @@
-import { Component } from "react";
+import { Component, ErrorInfo, ReactNode } from "react";
 import { Link, Redirect } from "react-router-dom";
 
 class ErrorBoundary extends Component {
   state = { hasError: false, redirect: false };
-  static getDerivedStateFromError() {
-    return { hasError: true };
+
+  //we expect return type as an object containing hasError and redirect both of type boolean
+  static getDerivedStateFromError(): { hasError: boolean; redirect: boolean } {
+    return { hasError: true, redirect: false };
   }
 
-  componentDidCatch(error, info) {
+  componentDidCatch(error: Error, info: ErrorInfo): void {
     //Log this to Sentry, New Relic, TrackJs
     console.error("ErrorBoundary caught an error", error, info);
     setTimeout(() => this.setState({ redirect: true }), 5000);
   }
 
-  render() {
+  componentDidUpdate(): void {
+    if (this.state.hasError) {
+      setTimeout(() => this.setState({ redirect: true }), 5000);
+    }
+  }
+
+  render(): ReactNode {
     if (this.state.redirect) {
       return <Redirect to="/" />;
     }
