ef6ead2671a8489e2251630734f6ba97668a2671
Rajat Bansal <rjtbansal@gmail.com>
2021-06-12 11:41:15 -0400

    created APIResponseTypes and migrated Details to TS

--- src/Details.tsx
+++ src/Details.tsx
@@ -1,13 +1,29 @@
-import { useContext, useEffect, useState } from "react";
-import { withRouter } from "react-router";
+import { FunctionComponent, useContext, useEffect, useState } from "react";
+import { withRouter, RouteComponentProps } from "react-router";
 import Carousel from "./Carousel";
 import ErrorBoundary from "./ErrorBoundary";
 import Modal from "./Modal";
 import ThemeContext from "./ThemeContext";
+import { PetAPIResponse, Animal } from "./APIResponseTypes";
 
-const Details = (props) => {
+/** below Details is our FuncionComponent type which accepts RouteComponentProps which in itself expect a request parameter of id of type string
+ * Look at App.js to see :id being passed to Details component through routing
+ */
+const Details: FunctionComponent<RouteComponentProps<{ id: string }>> = (
+  props
+) => {
   const [loading, setLoading] = useState(true);
-  const [petDetails, setPetDetails] = useState({});
+  //being more specific about our petDetails
+  const [petDetails, setPetDetails] = useState({
+    animal: "" as Animal, //except animal response to be of type Animal
+    breed: "",
+    city: "",
+    state: "",
+    description: "",
+    name: "",
+    images: [] as string[], //expecting images to be array of strings urls
+  });
+
   const [theme] = useContext(ThemeContext);
   const [showModal, setShowModal] = useState(false);
 
@@ -16,7 +32,8 @@ const Details = (props) => {
       const res = await fetch(
         `http://pets-v2.dev-apis.com/pets?id=${props.match.params.id}`
       );
-      const json = await res.json();
+      //expecting our response to have a shape similar to PetAPIResponse
+      const json = (await res.json()) as PetAPIResponse;
       setLoading(false);
       setPetDetails(json.pets[0]);
     }
@@ -28,7 +45,7 @@ const Details = (props) => {
   //throw new Error; //uncomment to actually trigger the error by our ErrorBoundary
 
   const toggleModal = () => setShowModal(!showModal);
-  const adopt = () => (window.location = "http://bit.ly/pet-adopt");
+  const adopt = () => (window.location.href = "http://bit.ly/pet-adopt");
 
   return loading ? (
     <h2>Loading...</h2>
@@ -62,11 +79,24 @@ const Details = (props) => {
 };
 
 const DetailsWithRouter = withRouter(Details);
+
+const DetailsErrorBoundary: FunctionComponent =
+  //we are assigning a named function so it shows up in stacktrace
+  function DetailsWithErrorBoundary() {
+    return (
+      <ErrorBoundary>
+        <DetailsWithRouter />
+      </ErrorBoundary>
+    );
+  };
+
+export default DetailsErrorBoundary;
+  
 //we need withRouter HOC because react-router-dom needs to pass us our match.params.id
-export default function DetailsWithErrorBoundary() {
-  return (
-    <ErrorBoundary>
-      <DetailsWithRouter />
-    </ErrorBoundary>
-  );
-}
+// export default function DetailsWithErrorBoundary(props) {
+//   return (
+//     <ErrorBoundary>
+//       <DetailsWithRouter {...props}/>
+//     </ErrorBoundary>
+//   );
+// }
